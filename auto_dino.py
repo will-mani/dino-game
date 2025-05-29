8df5efcd903f2fbe63e7d4325cc580dc3ce63275
William Manirakiza <william.thibaud02@gmail.com>
2025-05-29 00:33:58 -0400

    Changed the jump conditions, refactored some code, and achieved the best results yet. Flying obstacles at high heights are a pain right now.

--- auto_dino.py
+++ auto_dino.py
@@ -7,31 +7,36 @@ from pynput import keyboard
 
 class dino:
     def __init__(self):
-        self.min_obstacle_x = None
-        self.obstacle_min_dino_height_percentage = 0.4
+        self.right_most_point = None
+        self.left_most_point = None
+        self.width = None
 
-        self.max_obstacle_y = None
-        self.obstacle_max_dino_height_percentage = 1.1
+        self.prev_top_point = None
+        self.top_point = None
+        self.bottom_point = None
+        self.height = None
 
-        self.min_obstacle_y = None
+        self.model_contour = self.generate_model_contour()
+        self.curr_contour = None
+
         self.last_off_gorund_timestamp = 0
-
-        self.model_dino_contour = self.generate_model_dino_contour()
-        self.curr_dino_contour = None
-        self.prev_dino_contour = None
+    
+        self.obstacle_view_min_x= None
+        self.obstacle_view_min_dino_height_percentage = 0.4
+        self.obstacle_view_max_y = None
+        self.obstacle_view_max_dino_height_percentage = 1.2
+        self.obstacle_view_min_y = None
 
         self.obstacle_view_thresh = None
 
-        self.is_ascending_to_top = False
+        self.is_ascending_to_min_y = False
         self.ascent_start_timestamp = None
-        self.secs_to_top_of_obstacle = 0
-
-        self.is_over_obstacle = False
-        self.secs_over_obstacle = 0
-        self.over_obstacle_start_timestamp = None
+        self.secs_to_top_of_min_y = 0
+        self.is_over_min_y = False 
+        # min_y is short for obstacle_view_min_y
 
 
-    def generate_model_dino_contour(self): 
+    def generate_model_contour(self): 
         model_dino_img = cv2.imread('imgs/dino.png', cv2.IMREAD_GRAYSCALE)
         model_dino_img = cv2.resize(model_dino_img, (0, 0), fx = 0.5, fy = 0.5)
         _, thresh = cv2.threshold(model_dino_img, 127, 255,0)
@@ -40,122 +45,77 @@ class dino:
         return model_dino_contour
 
 
-    def approx_game_dino_contour(self, game_thresh):
-        
+    def approx_game_contour(self, game_thresh):
         game_contours, _ = cv2.findContours(game_thresh, 2, 1)
         min_difference = float('inf')
-        for contour in game_contours:
-            difference = cv2.matchShapes(self.model_dino_contour, contour, 1, 0.0)
+        for potential_contour in game_contours:
+            difference = cv2.matchShapes(self.model_contour, potential_contour, 1, 0.0)
             if difference < min_difference:
-                self.curr_dino_contour = contour
+                self.curr_contour = potential_contour
                 min_difference = difference
         
         if min_difference > 0.1:
-            self.curr_dino_contour = None
+            self.curr_contour = None
 
-        return self.curr_dino_contour
-
-
-    def calculate_dino_contour_properties(self, dino_contour):
-        properties_dict = {}
-        reshaped_contour = np.reshape(dino_contour, (dino_contour.shape[0], dino_contour.shape[2]))
-        
-        properties_dict["right_most_point"] = max(reshaped_contour[:, 0])
-        properties_dict["left_most_point"] = min(reshaped_contour[:, 0])
-        properties_dict["dino_width"] = properties_dict["right_most_point"] - properties_dict["left_most_point"]
-        
-        properties_dict["top_point"] = min(reshaped_contour[:, 1])
-        properties_dict["bottom_point"] = max(reshaped_contour[:, 1])
-        properties_dict["dino_height"] =  properties_dict["bottom_point"] - properties_dict["top_point"]
-
-        return properties_dict
-
+        return self.curr_contour
     
-    def get_game_dino_width(self):
-        game_dino_properties_dict = self.calculate_dino_contour_properties(self.curr_dino_contour)
-        return game_dino_properties_dict["dino_width"]
-
-
-    def is_on_ground(self):
-        curr_top_point = self.calculate_dino_contour_properties(self.curr_dino_contour)["top_point"]
-        if self.prev_dino_contour is None:
-            prev_top_point = curr_top_point
-        else:
-            prev_top_point = self.calculate_dino_contour_properties(self.prev_dino_contour)["top_point"] 
-        y_position_delta = abs(prev_top_point - curr_top_point)
-        secs_on_ground = time.time() - self.last_off_gorund_timestamp
-        return y_position_delta == 0 and secs_on_ground > 1
-
-
-    def update_obstacle_view(self, game_thresh):
-        self.obstacle_view_thresh = game_thresh.copy()
-        self.obstacle_view_thresh[:, :self.min_obstacle_x] = 127
-        self.obstacle_view_thresh[self.max_obstacle_y:, :] = 127
-        self.obstacle_view_thresh[:self.min_obstacle_y, :] = 127
-        cv2.drawContours(self.obstacle_view_thresh, [self.curr_dino_contour], 0, 255, -1)
-
-
-    def nearest_obstacle_start(self, game_thresh):
-        game_dino_properties_dict = self.calculate_dino_contour_properties(self.curr_dino_contour)
-        left_most_point = game_dino_properties_dict["left_most_point"]
-        top_point = game_dino_properties_dict["top_point"]
-        dino_height = game_dino_properties_dict["dino_height"]
+    def update_contour_properties(self):
+        reshaped_contour = np.reshape(self.curr_contour, (self.curr_contour.shape[0], self.curr_contour.shape[2]))
         
-        if self.is_on_ground():
-            self.min_obstacle_x = left_most_point
-            self.max_obstacle_y = top_point + int(dino_height * (1 - self.obstacle_min_dino_height_percentage))
-            self.min_obstacle_y = top_point - int(dino_height * (self.obstacle_max_dino_height_percentage  - 1))
+        self.right_most_point = max(reshaped_contour[:, 0])
+        self.left_most_point = min(reshaped_contour[:, 0])
+        self.width = self.right_most_point - self.left_most_point
+
+        self.prev_top_point = self.top_point
+        self.top_point = min(reshaped_contour[:, 1])
+        self.bottom_point = max(reshaped_contour[:, 1])
+        self.height = self.bottom_point - self.top_point
+
+
+    def is_defenitely_on_ground(self):
+        if self.prev_top_point == None:
+            self.prev_top_point = self.top_point
+        y_position_delta = abs(self.prev_top_point - self.top_point)
+        secs_on_ground = time.time() - self.last_off_gorund_timestamp
+        return y_position_delta == 0 and secs_on_ground > 2
+    
+    
+    def update_obstacle_view_properties(self, game_thresh):
+        if self.is_defenitely_on_ground():
+            self.obstacle_view_min_x = self.left_most_point
+            self.obstacle_view_max_y = self.bottom_point - int(self.height * self.obstacle_view_min_dino_height_percentage)
+            self.obstacle_view_min_y = self.bottom_point - int(self.height * self.obstacle_view_max_dino_height_percentage)
         else:
             self.last_off_gorund_timestamp = time.time()
 
-        self.update_obstacle_view(game_thresh)
+        self.obstacle_view_thresh = game_thresh.copy()
+        self.obstacle_view_thresh[:, :self.obstacle_view_min_x] = 127
+        self.obstacle_view_thresh[self.obstacle_view_max_y:, :] = 127
+        self.obstacle_view_thresh[:self.obstacle_view_min_y, :] = 127
+        cv2.drawContours(self.obstacle_view_thresh, [self.curr_contour], 0, 255, -1)
 
-        obstacle_x_coors = np.where(self.obstacle_view_thresh == 0)[1]
-        if len(obstacle_x_coors) >= 1:
-            obstacle_start_x = min(obstacle_x_coors)
-        else:
-            obstacle_start_x = None
 
-        self.prev_dino_contour = self.curr_dino_contour.copy()
-
-        return obstacle_start_x
-
-    
     def jump_now(self, nearest_obstacle):
-        game_dino_properties_dict = self.calculate_dino_contour_properties(self.curr_dino_contour)
-        left_most_point = game_dino_properties_dict["left_most_point"]
-        right_most_point = game_dino_properties_dict["right_most_point"]
-        bottom_point = game_dino_properties_dict["bottom_point"]
+        if (not self.is_ascending_to_min_y and not self.is_over_min_y
+            and self.right_most_point < nearest_obstacle.start_x and self.top_point < nearest_obstacle.bottom):
 
-        if not self.is_ascending_to_top and not self.is_over_obstacle and right_most_point < nearest_obstacle.start_x:
-            approx_obstacle_velocity = nearest_obstacle.pixels_per_sec
+            adjusted_obstacle_velocity = nearest_obstacle.pixels_per_sec * 1.5
 
-            # approximate end of the obstacle after dino reaches its top
-            adjusted_obstacle_end_x = nearest_obstacle.end_x - (approx_obstacle_velocity * ((self.secs_to_top_of_obstacle / (nearest_obstacle.prev_top_point - bottom_point)) * (nearest_obstacle.top_point - bottom_point)))
-
-            if left_most_point > (adjusted_obstacle_end_x - (approx_obstacle_velocity * ((self.secs_over_obstacle / (nearest_obstacle.prev_end_x - nearest_obstacle.prev_start_x)) * (nearest_obstacle.end_x - nearest_obstacle.start_x)))):
-
-                print(self.secs_to_top_of_obstacle)
-
+            if (nearest_obstacle.start_x - (adjusted_obstacle_velocity * self.secs_to_top_of_min_y)) <= self.right_most_point:
                 return True
 
         return False
 
     
-    def update_jump_properties(self, nearest_obstacle):
-        bottom_point = self.calculate_dino_contour_properties(self.curr_dino_contour)["bottom_point"]
-        if bottom_point < nearest_obstacle.top_point:
-            self.is_ascending_to_top = False
-            if self.is_over_obstacle:
-                self.secs_over_obstacle = time.time() - self.over_obstacle_start_timestamp
-            else:
-                self.is_over_obstacle = True
-                self.over_obstacle_start_timestamp = time.time()
+    def update_jump_properties(self):
+        if self.bottom_point < self.obstacle_view_min_y:
+            self.is_ascending_to_min_y = False
+            self.is_over_min_y = True
         else:
-            self.is_over_obstacle = False
+            self.is_over_min_y = False
 
-        if self.is_ascending_to_top:
-            self.secs_to_top_of_obstacle = time.time() - self.ascent_start_timestamp
+        if self.is_ascending_to_min_y:
+            self.secs_to_top_of_min_y = time.time() - self.ascent_start_timestamp
 
 
 
@@ -163,11 +123,8 @@ class obstacle:
     def __init__(self):
         self.start_x = None
         self.end_x = None
-        self.top_point = None
-
-        self.prev_start_x = None
-        self.prev_end_x = None
-        self.prev_top_point = None
+        self.top = None
+        self.bottom = None
         
         self.reached_dino_right_most_point = False
         self.dino_right_most_point_timestamp = None
@@ -175,21 +132,34 @@ class obstacle:
         self.previously_in_collision_range = False
         self.pixels_per_sec = 0
 
+
     def update_position_properties(self, dino, game_thresh):
-        self.start_x = dino.nearest_obstacle_start(game_thresh)
+        self.start_x = self.calculate_start_x(dino.obstacle_view_thresh)
         if self.start_x == None:
             self.start_x = game_thresh.shape[1] - 1
             self.end_x = self.start_x
-            self.top_point = 0
+            self.top = 0
+            self.bottom = game_thresh.shape[0] - 1
         else:
-            self.end_x = self.calculate_end_x(dino.obstacle_view_thresh, dino.get_game_dino_width())
-            self.top_point = self.calculate_top_point(dino.max_obstacle_y, game_thresh)
+            self.end_x = self.calculate_end_x(dino.obstacle_view_thresh, dino.width)
+            self.top, self.bottom = self.calculate_top_and_bottom(dino.obstacle_view_thresh)
 
-    def calculate_end_x(self, obstacle_view_thresh, game_dino_width):
+    
+    def calculate_start_x(self, obstacle_view_thresh):
+        obstacle_x_coors = np.where(obstacle_view_thresh == 0)[1]
+        if len(obstacle_x_coors) >= 1:
+            obstacle_start_x = min(obstacle_x_coors)
+        else:
+            obstacle_start_x = None
+
+        return obstacle_start_x
+    
+
+    def calculate_end_x(self, obstacle_view_thresh, dino_width):
         curr_end_x = self.start_x
         while True:
             window_start = curr_end_x + 1
-            window_end = window_start + game_dino_width
+            window_end = window_start + dino_width
             obstacle_window_x_coors = np.where(obstacle_view_thresh[:, window_start:window_end] == 0)[1] + window_start
             if len(obstacle_window_x_coors) >= 1:
                 curr_end_x = max(obstacle_window_x_coors)
@@ -197,21 +167,18 @@ class obstacle:
                 break
                 
         return curr_end_x
+    
 
-    def calculate_top_point(self, min_top_point, game_thresh):
-        curr_top_point = min_top_point
-        while True:
-            curr_top_point -= 1
-            obstacle_window_row = game_thresh[curr_top_point, self.start_x:self.end_x]
-            if len(np.where(obstacle_window_row ==0)[0]) == 0:
-                break
-        
-        return curr_top_point
+    def calculate_top_and_bottom(self, obstacle_view_thresh):
+        window_end = self.end_x + 1
+        obstacle_view_window = obstacle_view_thresh[:, :window_end]
+        obstacle_y_coors = np.where(obstacle_view_window == 0)[0]
+        obstacle_top_point = min(obstacle_y_coors)
+        obstacle_bottom_point = max(obstacle_y_coors)
+        return obstacle_top_point, obstacle_bottom_point
+    
 
-    def update_pixels_per_sec(self, dino):
-        game_dino_properties_dict = dino.calculate_dino_contour_properties(dino.curr_dino_contour)
-        dino_right_most_point = game_dino_properties_dict["right_most_point"]
-
+    def update_pixels_per_sec(self, dino_right_most_point, dino_width):
         if not self.reached_dino_right_most_point and self.start_x <= dino_right_most_point:
             self.reached_dino_right_most_point = True
             self.dino_right_most_point_timestamp = time.time()
@@ -222,14 +189,16 @@ class obstacle:
         
         if self.previously_in_collision_range and self.start_x > dino_right_most_point:
             time_passed = time.time() - self.dino_right_most_point_timestamp
-            dino_width = game_dino_properties_dict["dino_width"]
             x_delta = self.width_at_dino_right_most_point + dino_width
             self.pixels_per_sec = x_delta / time_passed
 
         self.previously_in_collision_range = self.reached_dino_right_most_point
 
+
+
 global jump_count
 jump_count = 0
+
 
 def capture_screen(image_grab_bbox):
     screenshot = ImageGrab.grab(bbox=image_grab_bbox)
@@ -248,18 +217,13 @@ def revert_colors(thresh_img):
 
 def on_press(key):
     global jump_count
-    global nearest_obstacle
     
     if key == keyboard.Key.up or key == keyboard.Key.space:
         print("Jump Time!")
-        rex.is_ascending_to_top = True
+        rex.is_ascending_to_min_y = True
         rex.ascent_start_timestamp = time.time()
 
         jump_count += 1
-
-        nearest_obstacle.prev_start_x = nearest_obstacle.start_x
-        nearest_obstacle.prev_end_x = nearest_obstacle.end_x
-        nearest_obstacle.prev_top_point = nearest_obstacle.top_point
 
 
 keyboard_listener = keyboard.Listener(on_press=on_press)
@@ -271,10 +235,9 @@ whole_screen = capture_screen(image_grab_bbox=None)
 roi = cv2.selectROI("ROI", whole_screen) 
 
 rex = dino()
-global nearest_obstacle
 nearest_obstacle = obstacle()
 
-auto_pilot = False # human starts game off in control to set: obstacle.pixels_per_sec, dino.secs_to_top_of_obstacle, and dino.secs_over_obstacle
+auto_pilot = False # human starts game off in control to set: obstacle.pixels_per_sec and dino.secs_to_top_of_min_y
 
 # frame_count = 0
 # sec_start = time.time()
@@ -293,28 +256,33 @@ while True:
     if game_thresh[0, 0] == 0:
         game_thresh = revert_colors(game_thresh)
 
-    game_dino_contour = rex.approx_game_dino_contour(game_thresh)
+    game_dino_contour = rex.approx_game_contour(game_thresh)
 
     if game_dino_contour is not None:
 
-        nearest_obstacle.update_position_properties(rex, game_thresh)
-        nearest_obstacle.update_pixels_per_sec(rex)
+        rex.update_contour_properties()
+        rex.update_obstacle_view_properties(game_thresh)
 
-        rex.update_jump_properties(nearest_obstacle)
+        nearest_obstacle.update_position_properties(rex, game_thresh)
+        nearest_obstacle.update_pixels_per_sec(rex.right_most_point, rex.width)
+
+        rex.update_jump_properties()
 
         if auto_pilot:
             if rex.jump_now(nearest_obstacle):
                 pyautogui.press('up')
 
-        cv2.line(game_thresh, (nearest_obstacle.start_x, nearest_obstacle.top_point), (nearest_obstacle.start_x, (game_thresh.shape[0] - 1)), 127 * (not rex.is_over_obstacle), 3)
-        cv2.line(game_thresh, (nearest_obstacle.end_x, nearest_obstacle.top_point), (nearest_obstacle.end_x, (game_thresh.shape[0] - 1)), 127 * (not rex.is_over_obstacle), 3)
-        cv2.line(game_thresh, (nearest_obstacle.start_x, nearest_obstacle.top_point), (nearest_obstacle.end_x, nearest_obstacle.top_point), 127 * (not rex.is_over_obstacle), 3)
+        cv2.line(game_thresh, (nearest_obstacle.start_x, nearest_obstacle.top), (nearest_obstacle.start_x, nearest_obstacle.bottom), 127, 3)
+        cv2.line(game_thresh, (nearest_obstacle.end_x, nearest_obstacle.top), (nearest_obstacle.end_x, nearest_obstacle.bottom), 127, 3)
+        cv2.line(game_thresh, (nearest_obstacle.start_x, nearest_obstacle.top), (nearest_obstacle.end_x, nearest_obstacle.top), 127, 3)
+        cv2.line(game_thresh, (0, rex.obstacle_view_min_y), (game_thresh.shape[1], rex.obstacle_view_min_y), 0, 1)
+
         cv2.imshow("Obstacle View", rex.obstacle_view_thresh)
     
     cv2.imshow("ROI", game_thresh)
 
     k = cv2.waitKey(1) & 0xFF
-    if k == ord('q'):
+    if k == 27:
         break
 
     # enabling auto_pilot
