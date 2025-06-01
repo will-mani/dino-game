823668218affa7a094a0ce5b87395c1c11c962c4
William Manirakiza <william.thibaud02@gmail.com>
2025-05-31 23:57:17 -0400

    Closer, but not there. Main changes to obstacle velocity, jump condition, and defenitely on ground (which was not at all working). To improve, adjusted position might have to be weighted by velocity instead of static decimal. Charge forward

--- auto_dino.py
+++ auto_dino.py
@@ -20,6 +20,9 @@ class dino:
         self.curr_contour = None
 
         self.last_off_gorund_timestamp = 0
+        self.close_to_ground_min_y = None
+        self.close_to_ground_dino_height_percentage = 0.5
+        self.is_close_to_ground = True
     
         self.obstacle_view_min_x= None
         self.obstacle_view_min_dino_height_percentage = 0.4
@@ -32,7 +35,6 @@ class dino:
         self.is_ascending_to_min_y = False
         self.ascent_start_timestamp = None
         self.secs_to_top_of_min_y = 0
-        self.is_over_min_y = False 
         # min_y is short for obstacle_view_min_y
 
 
@@ -76,17 +78,19 @@ class dino:
         if self.prev_top_point == None:
             self.prev_top_point = self.top_point
         y_position_delta = abs(self.prev_top_point - self.top_point)
+        if y_position_delta > 0:
+            self.last_off_gorund_timestamp = time.time()
+            return False
         secs_on_ground = time.time() - self.last_off_gorund_timestamp
-        return y_position_delta == 0 and secs_on_ground > 2
+        return secs_on_ground > 2
     
     
     def update_obstacle_view_properties(self, game_thresh):
         if self.is_defenitely_on_ground():
-            self.obstacle_view_min_x = self.left_most_point
+            self.obstacle_view_min_x = self.right_most_point
             self.obstacle_view_max_y = self.bottom_point - int(self.height * self.obstacle_view_min_dino_height_percentage)
             self.obstacle_view_min_y = self.bottom_point - int(self.height * self.obstacle_view_max_dino_height_percentage)
-        else:
-            self.last_off_gorund_timestamp = time.time()
+            self.close_to_ground_min_y = self.bottom_point - int(self.height * self.close_to_ground_dino_height_percentage)
 
         self.obstacle_view_thresh = game_thresh.copy()
         self.obstacle_view_thresh[:, :self.obstacle_view_min_x] = 127
@@ -96,23 +100,28 @@ class dino:
 
 
     def jump_now(self, nearest_obstacle):
-        if (not self.is_ascending_to_min_y and not self.is_over_min_y
+        if (not self.is_ascending_to_min_y and self.is_close_to_ground 
             and self.right_most_point < nearest_obstacle.start_x and self.top_point < nearest_obstacle.bottom):
 
-            adjusted_obstacle_velocity = nearest_obstacle.pixels_per_sec * 1.5
+            adjusted_obstacle_start = nearest_obstacle.start_x * 0.8
+            adjusted_obstacle_velocity = nearest_obstacle.pixels_per_sec * 1.25
 
-            if (nearest_obstacle.start_x - (adjusted_obstacle_velocity * self.secs_to_top_of_min_y)) <= self.right_most_point:
+            # print(adjusted_obstacle_velocity)
+
+            if (adjusted_obstacle_start - (adjusted_obstacle_velocity * self.secs_to_top_of_min_y)) <= self.right_most_point:
                 return True
 
         return False
 
     
     def update_jump_properties(self):
+        if self.bottom_point >= self.close_to_ground_min_y:
+            self.is_close_to_ground = True
+        else:
+            self.is_close_to_ground = False
+    
         if self.bottom_point < self.obstacle_view_min_y:
             self.is_ascending_to_min_y = False
-            self.is_over_min_y = True
-        else:
-            self.is_over_min_y = False
 
         if self.is_ascending_to_min_y:
             self.secs_to_top_of_min_y = time.time() - self.ascent_start_timestamp
@@ -126,21 +135,26 @@ class obstacle:
         self.top = None
         self.bottom = None
         
-        self.reached_dino_right_most_point = False
-        self.dino_right_most_point_timestamp = None
-        self.width_at_dino_right_most_point = None
-        self.previously_in_collision_range = False
         self.pixels_per_sec = 0
+        self.can_update_velocity = True
+
+        self.is_visible = False
+        self.last_visible_end_x = float('inf')
+        self.respawn_timestamp = None
+        self.respawn_start_x = None
+        self.velocity_dict = {"secs_passed_list":[], "displacement_list":[]}
 
 
     def update_position_properties(self, dino, game_thresh):
         self.start_x = self.calculate_start_x(dino.obstacle_view_thresh)
         if self.start_x == None:
+            self.is_visible = False
             self.start_x = game_thresh.shape[1] - 1
             self.end_x = self.start_x
             self.top = 0
             self.bottom = game_thresh.shape[0] - 1
         else:
+            self.is_visible = True
             self.end_x = self.calculate_end_x(dino.obstacle_view_thresh, dino.width)
             self.top, self.bottom = self.calculate_top_and_bottom(dino.obstacle_view_thresh)
 
@@ -178,23 +192,26 @@ class obstacle:
         return obstacle_top_point, obstacle_bottom_point
     
 
-    def update_pixels_per_sec(self, dino_right_most_point, dino_width):
-        if not self.reached_dino_right_most_point and self.start_x <= dino_right_most_point:
-            self.reached_dino_right_most_point = True
-            self.dino_right_most_point_timestamp = time.time()
-            self.width_at_dino_right_most_point = self.end_x - self.start_x
+    def update_pixels_per_sec(self):
+        if self.is_visible:
+            if self.end_x > self.last_visible_end_x and len(self.velocity_dict['secs_passed_list']) > 1:
+                x, y = self.velocity_dict['secs_passed_list'], self.velocity_dict['displacement_list']
+                # best_fit_slope, _ = np.polyfit(x, y, deg=1)
+                self.pixels_per_sec = y[-1] / x[-1]# best_fit_slope
+                print(self.pixels_per_sec)
 
-        if self.start_x > dino_right_most_point:
-            self.reached_dino_right_most_point = False
-        
-        if self.previously_in_collision_range and self.start_x > dino_right_most_point:
-            time_passed = time.time() - self.dino_right_most_point_timestamp
-            x_delta = self.width_at_dino_right_most_point + dino_width
-            self.pixels_per_sec = x_delta / time_passed
+            if self.end_x > self.last_visible_end_x or self.respawn_timestamp == None:
+                self.respawn_timestamp = time.time()
+                self.respawn_start_x = self.start_x
+                self.velocity_dict['secs_passed_list'] = [0]
+                self.velocity_dict['displacement_list'] = [0]
+            else:
+                secs_passed = time.time() - self.respawn_timestamp
+                displacement = self.respawn_start_x - self.start_x
+                self.velocity_dict['secs_passed_list'].append(secs_passed)
+                self.velocity_dict['displacement_list'].append(displacement)
 
-        self.previously_in_collision_range = self.reached_dino_right_most_point
-
-
+            self.last_visible_end_x = self.end_x
 
 global jump_count
 jump_count = 0
@@ -264,13 +281,14 @@ while True:
         rex.update_obstacle_view_properties(game_thresh)
 
         nearest_obstacle.update_position_properties(rex, game_thresh)
-        nearest_obstacle.update_pixels_per_sec(rex.right_most_point, rex.width)
+        nearest_obstacle.update_pixels_per_sec()
 
         rex.update_jump_properties()
 
         if auto_pilot:
             if rex.jump_now(nearest_obstacle):
                 pyautogui.press('up')
+                pyautogui.keyUp('up')
 
         cv2.line(game_thresh, (nearest_obstacle.start_x, nearest_obstacle.top), (nearest_obstacle.start_x, nearest_obstacle.bottom), 127, 3)
         cv2.line(game_thresh, (nearest_obstacle.end_x, nearest_obstacle.top), (nearest_obstacle.end_x, nearest_obstacle.bottom), 127, 3)
